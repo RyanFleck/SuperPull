5e7aa7ff2f95432bca817cfdba59a28070a046d0
Ryan Fleck <ryan.fleck@protonmail.com>
2019-01-26 12:40:10 -0500

    Finish directory-adding functionality.

--- superpull.js
+++ superpull.js
@@ -25,6 +25,7 @@
 const prog = require('commander');
 const chalk = require('chalk');
 const fs = require('fs');
+const path = require('path');
 const exec = require('child_process').exec;
 const config = require('os').homedir().concat('/.superpull');
 
@@ -52,10 +53,12 @@ const getArrayOfDirs = () => {
     return fs.readFileSync(config).toString().split('\n').filter(x=>x!='');
 };
 
-console.log(getArrayOfDirs());
+const checkIfAlreadyInFile = (fullpath) => {
+    return fs.readFileSync(config).toString().split('\n').some(x => x === fullpath);
+}
 
 const addDirToConfig = (fullpath) => {
-    fs.appendFileSync(config, '\n'.concat(fullpath), (e) => {
+    fs.appendFileSync(config, '\n'.concat(fullpath.trim()), (e) => {
         console.log(`Failed to modify ~/.superpull.\nFull path: ${config}\nDir: ${fullpath}\n${e}`);
         process.exit();
     });
@@ -69,29 +72,59 @@ const dirIsGitRepo = (path) => {
     }
 }
 
+const prettyPrintConfig = () => {
+    const msg = '\nSuperPull Repositories:\n';
+    console.log(msg+'-'.repeat(msg.trim().length));
+    getArrayOfDirs().forEach( (dir,i) => {
+        console.log(`${chalk.yellow(i)} ${chalk.green(path.basename(dir))} ${chalk.yellow('@')} ${dir}`); 
+    });
+    console.log('\n');
+}
+
+
 /* Section Three:
  *   Program Functions.
  */
 
 const addDir = (dir, cmd) => {
-    console.log(`${chalk.red('Add Dir to Config')}!`);
-
     console.log(dir);
-    console.log(cmd);
+    console.log(`${chalk.red('Add Dir to Config')}!`);
+    console.log(`Add is a string? >> ${(typeof dir.add === 'string')}`);
+    
+    let newDir = "";
+    
+    if (typeof dir.add === 'string'){
+        newDir = path.resolve(dir.add)
+    } else {
+        newDir = process.cwd();
+    }
+
+    console.log(`Adding ${newDir} to ~/.superpull`);
+    
+    if(!dirIsGitRepo(newDir.concat('/.git'))){
+        console.log(`Dir is ${chalk.red('NOT')} git repo.`); 
+        console.log(newDir);
+        process.exit(1);
+    }else{
+        console.log('Dir is git repo.'); 
+        console.log(newDir);
+        if(!checkIfAlreadyInFile(newDir)){
+            addDirToConfig(newDir);
+        }else{
+            console.log('Dir already in file!'); 
+            process.exit(1);
+        }
+        process.exit(0);
+    }
 };
 
 const listDirs = (dir, cmd) => {
-    console.log(`${chalk.yellow('List Config Dirs')}!`);
-
-    console.log(dir);
-    console.log(cmd);
+    prettyPrintConfig();
 };
 
 const superPull = (dir,cmd) => {
-    console.log(`${chalk.green('SuperPull')}!`);
-
     console.log(dir);
-    console.log(cmd);
+    console.log(`${chalk.green('SuperPull')}!`);
 };
 
 const main = (dir, cmd) => {
